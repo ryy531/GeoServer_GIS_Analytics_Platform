e12a2992e6bf4d7214b9b48842dad0e851f6357b
Ruby Yang <RubyYang960531@gmail.com>
2025-08-29 16:46:21 -0600

    edited deploy online code

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 // File: frontend/src/services/apiService.js
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 /**
  * Fetches nearby facilities from the backend.
